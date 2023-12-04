cf932606b2f5b2ed5c4f36ba4a57b3aee665b1b4
mohanson <mohanson@outlook.com>
2023-12-04 11:56:34 +0800

    Rewrite queryBalance by indexer rpc

--- dapp/src/tmQuery.ts
+++ dapp/src/tmQuery.ts
@@ -1,7 +1,9 @@
-import { Script, Indexer, BI, HexString, OutPoint } from "@ckb-lumos/lumos";
+import { Script, Indexer, BI, HexString, OutPoint, RPC } from "@ckb-lumos/lumos";
 import { config } from './tmConfig';
 import { tmAccounts } from "./tmWallet";
 import { accounts as skAccounts } from "./skWallet";
+
+
 const indexer = new Indexer(config.ckbIndexerUrl);
 const spore = config.scripts.Spore.script;
 
@@ -21,20 +23,16 @@ async function querySporeCells(lock: Script): Promise<Array<{ args: HexString, o
 }
 
 // It costs a lot of time if a owner has lots of UTXO
-async function queryBalance(lock: Script): Promise<[BI, boolean]> {
-    const collector = indexer.collector({ lock });
-    let balance: BI = BI.from(0);
-    let count = 0;
-    let overflow = false;
-    for await (const cell of collector.collect()) {
-        balance = balance.add(cell.cellOutput.capacity)
-        count += 1;
-        if (count > 256) {
-            overflow = true;
-            break
+async function queryBalance(lock: Script): Promise<BI> {
+    const rpc = new RPC(config.ckbNodeUrl);
+    let r = await rpc.getCellsCapacity({
+        script: lock,
+        scriptType: 'lock',
+        filter: {
+            scriptLenRange: ['0x0', '0x1']
         }
-    }
-    return [balance.div(BI.from(10).pow(8)), overflow];
+    })
+    return BI.from(r.capacity).div(BI.from(10).pow(8))
 }
 
 async function main() {
@@ -48,18 +46,10 @@ async function main() {
     for (let cell of bob_cells) {
         console.log("outPoint = ", cell.outPoint, "args =", cell.args);
     }
-    let [alice_balance, alice_overflow] = await queryBalance(skAccounts.alice.lock);
-    if (alice_overflow) {
-        console.log(`alice's balance is more than ${alice_balance} (too many UTXO, abort early)`);
-    } else {
-        console.log(`alice's balance is ${alice_balance}`)
-    }
-    let [bob_balance, bob_overflow] = await queryBalance(skAccounts.bob.lock);
-    if (bob_overflow) {
-        console.log(`bob's balance is more than ${bob_balance} (too many UTXO, abort early)`);
-    } else {
-        console.log(`bob's balance is ${bob_balance}`)
-    }
+    let alice_balance = await queryBalance(skAccounts.alice.lock);
+    console.log(`alice's balance is ${alice_balance} CKByte`)
+    let bob_balance = await queryBalance(skAccounts.bob.lock);
+    console.log(`bob's balance is ${bob_balance} CKByte`)
 }
 
 main();
