ae6d9924eda18a2ab2ef132401717667e9e518e6
quake <quake.wang@gmail.com>
2023-12-27 14:36:37 +0900

    chore: add SighashAllOnly layout support

--- ckb-transaction-cobuild/src/lib.rs
+++ ckb-transaction-cobuild/src/lib.rs
@@ -4,7 +4,7 @@ pub mod blake2b;
 pub mod schemas;
 
 use alloc::vec::Vec;
-use blake2b::new_sighash_all_blake2b;
+use blake2b::{new_sighash_all_blake2b, new_sighash_all_only_blake2b};
 use ckb_std::{
     ckb_constants::Source,
     ckb_types::packed::CellInput,
@@ -19,8 +19,8 @@ use molecule::{
     NUMBER_SIZE,
 };
 use schemas::{
-    basic::{Message, SighashAll},
-    top_level::{WitnessLayout, WitnessLayoutReader, WitnessLayoutUnion, WitnessLayoutUnionReader},
+    basic::Message,
+    top_level::{WitnessLayoutReader, WitnessLayoutUnionReader},
 };
 
 #[derive(Eq, PartialEq, Debug, Clone, Copy)]
@@ -44,16 +44,15 @@ impl From<VerificationError> for Error {
 }
 
 ///
-/// Fetch the corresponding WitnessLayout(SighashAll or SighashAllOnly)
-/// Used by lock script
+/// fetch the seal field of SighashAll or SighashAllOnly in current script group
 ///
-pub fn fetch_witness_layout() -> Result<WitnessLayout, Error> {
+fn fetch_seal() -> Result<Vec<u8>, Error> {
     match load_witness(0, Source::GroupInput) {
         Ok(witness) => {
             if let Ok(r) = WitnessLayoutReader::from_slice(&witness) {
                 match r.to_enum() {
-                    WitnessLayoutUnionReader::SighashAll(_)
-                    | WitnessLayoutUnionReader::SighashAllOnly(_) => Ok(r.to_entity()),
+                    WitnessLayoutUnionReader::SighashAll(s) => Ok(s.seal().raw_data().to_vec()),
+                    WitnessLayoutUnionReader::SighashAllOnly(s) => Ok(s.seal().raw_data().to_vec()),
                     _ => Err(Error::MoleculeEncoding),
                 }
             } else {
@@ -65,22 +64,23 @@ pub fn fetch_witness_layout() -> Result<WitnessLayout, Error> {
 }
 
 ///
-/// fetch the only SighashAll from all witnesses.
-/// This function can also check the count of SighashAll is one.
-///
-pub fn fetch_sighash_all() -> Result<SighashAll, Error> {
+/// fetch the message field of SighashAll
+/// returns None if there is no SighashAll witness
+/// returns Error::WrongWitnessLayout if there are more than one SighashAll witness
+pub fn fetch_message() -> Result<Option<Message>, Error> {
     let mut iter = QueryIter::new(load_witness, Source::Input).filter_map(|witness| {
         WitnessLayoutReader::from_slice(&witness)
             .ok()
             .and_then(|r| match r.to_enum() {
-                WitnessLayoutUnionReader::SighashAll(s) => Some(s.to_entity()),
+                WitnessLayoutUnionReader::SighashAll(s) => Some(s.message().to_entity()),
                 _ => None,
             })
     });
 
     match (iter.next(), iter.next()) {
-        (Some(sighash_with_action), None) => Ok(sighash_with_action),
-        _ => Err(Error::WrongSighashAll),
+        (Some(message), None) => Ok(Some(message)),
+        (None, None) => Ok(None),
+        _ => Err(Error::WrongWitnessLayout),
     }
 }
 
@@ -88,7 +88,7 @@ pub fn fetch_sighash_all() -> Result<SighashAll, Error> {
 /// for lock script with message, the other witness in script group except
 /// first one should be empty
 ///
-pub fn check_others_in_group() -> Result<(), Error> {
+fn check_others_in_group() -> Result<(), Error> {
     if QueryIter::new(load_witness, Source::GroupInput)
         .skip(1)
         .all(|witness| witness.is_empty())
@@ -99,10 +99,16 @@ pub fn check_others_in_group() -> Result<(), Error> {
     }
 }
 
-pub fn generate_signing_message_hash(message: &Message) -> Result<[u8; 32], Error> {
-    let mut hasher = new_sighash_all_blake2b();
+fn generate_signing_message_hash(message: &Option<Message>) -> Result<[u8; 32], Error> {
     // message
-    hasher.update(message.as_slice());
+    let mut hasher = match message {
+        Some(m) => {
+            let mut hasher = new_sighash_all_blake2b();
+            hasher.update(m.as_slice());
+            hasher
+        }
+        None => new_sighash_all_only_blake2b(),
+    };
     // tx hash
     hasher.update(&load_tx_hash()?);
     // inputs cell and data
@@ -123,7 +129,7 @@ pub fn generate_signing_message_hash(message: &Message) -> Result<[u8; 32], Erro
 
     let mut result = [0u8; 32];
     hasher.finalize(&mut result);
-    return Ok(result);
+    Ok(result)
 }
 
 ///
@@ -153,17 +159,8 @@ fn calculate_inputs_len() -> Result<usize, SysError> {
 ///
 pub fn parse_message() -> Result<([u8; 32], Vec<u8>), Error> {
     check_others_in_group()?;
-    // Ensure that a SighashAll is present throughout the entire transaction
-    let sighash_all = fetch_sighash_all()?;
-    // There are 2 possible values: SighashAllOnly or SighashAll
-    let witness = fetch_witness_layout()?;
-    let (lock, message) = match witness.to_enum() {
-        WitnessLayoutUnion::SighashAll(s) => (s.seal(), s.message()),
-        WitnessLayoutUnion::SighashAllOnly(s) => (s.seal(), sighash_all.message()),
-        _ => {
-            return Err(Error::WrongSighashAll);
-        }
-    };
+    let message = fetch_message()?;
     let signing_message_hash = generate_signing_message_hash(&message)?;
-    Ok((signing_message_hash, lock.raw_data().into()))
+    let seal = fetch_seal()?;
+    Ok((signing_message_hash, seal))
 }
